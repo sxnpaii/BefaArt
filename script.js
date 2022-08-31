ecadb10ee8cbee6635deabe17903eaf4af94d25e
Khurshid <axurshidbek2005@gmail.com>
2022-08-31 20:04:19 +0500

    BefaArt

---
+++ script.js
@@ -0,0 +1,323 @@
+let infoIcon1 = document.querySelector(".infoIcon1");
+let infoItem1 = document.querySelector(".info.i__1");
+
+let close1 = document.querySelector(".closeIcon1");
+let hoverDiv1 = document.querySelector('.hover.h__1')
+
+
+
+infoIcon1.addEventListener("click", function info_1(a) {
+    a.preventDefault();
+    hoverDiv1.classList.toggle("hidden");
+    setTimeout(() => {
+        infoItem1.classList.toggle("visible");
+    }, 300);
+
+});
+
+close1.addEventListener("click", function infoCls(b) {
+    b.preventDefault();
+    hoverDiv1.classList.toggle("hidden");
+    infoItem1.classList.toggle("visible");
+
+});
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+
+let infoIcon2 = document.querySelector(".infoIcon2");
+let infoItem2 = document.querySelector(".info.i__2");
+
+let close2 = document.querySelector(".closeIcon2");
+let hoverDiv2 = document.querySelector('.hover.h__2')
+
+
+
+infoIcon2.addEventListener("click", function info_2(a) {
+    a.preventDefault();
+    hoverDiv2.classList.toggle("hidden");
+    setTimeout(() => {
+        infoItem2.classList.toggle("visible");
+    }, 300);
+
+});
+
+close2.addEventListener("click", function infoCls(b) {
+    b.preventDefault();
+    hoverDiv2.classList.toggle("hidden");
+    infoItem2.classList.toggle("visible");
+
+});
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+let infoIcon3 = document.querySelector(".infoIcon3");
+let infoItem3 = document.querySelector(".info.i__3");
+
+let close3 = document.querySelector(".closeIcon3");
+let hoverDiv3 = document.querySelector('.hover.h__3')
+
+
+
+infoIcon3.addEventListener("click", function info_3(a) {
+    a.preventDefault();
+    hoverDiv3.classList.toggle("hidden");
+    setTimeout(() => {
+        infoItem3.classList.toggle("visible");
+    }, 300);
+
+});
+
+close3.addEventListener("click", function infoCls(b) {
+    b.preventDefault();
+    hoverDiv3.classList.toggle("hidden");
+    infoItem3.classList.toggle("visible");
+
+});
+
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+
+let infoIcon4 = document.querySelector(".infoIcon4");
+let infoItem4 = document.querySelector(".info.i__4");
+
+let close4 = document.querySelector(".closeIcon4");
+let hoverDiv4 = document.querySelector('.hover.h__4')
+
+
+
+infoIcon4.addEventListener("click", function info_4(a) {
+    a.preventDefault();
+    hoverDiv4.classList.toggle("hidden");
+    setTimeout(() => {
+        infoItem4.classList.toggle("visible");
+    }, 300);
+
+});
+
+close4.addEventListener("click", function infoCls(b) {
+    b.preventDefault();
+    hoverDiv4.classList.toggle("hidden");
+    infoItem4.classList.toggle("visible");
+
+});
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+
+let infoIcon5 = document.querySelector(".infoIcon5");
+let infoItem5 = document.querySelector(".info.i__5");
+
+let close5 = document.querySelector(".closeIcon5");
+let hoverDiv5 = document.querySelector('.hover.h__5')
+
+
+
+infoIcon5.addEventListener("click", function info_5(a) {
+    a.preventDefault();
+    hoverDiv5.classList.toggle("hidden");
+    setTimeout(() => {
+        infoItem5.classList.toggle("visible");
+    }, 300);
+
+});
+
+close5.addEventListener("click", function infoCls(b) {
+    b.preventDefault();
+    hoverDiv5.classList.toggle("hidden");
+    infoItem5.classList.toggle("visible");
+
+});
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+
+let infoIcon6 = document.querySelector(".infoIcon6");
+let infoItem6 = document.querySelector(".info.i__6");
+
+let close6 = document.querySelector(".closeIcon6");
+let hoverDiv6 = document.querySelector(".hover.h__6")
+
+
+
+infoIcon6.addEventListener("click", function info_6(a) {
+    a.preventDefault();
+    hoverDiv6.classList.toggle("hidden");
+    setTimeout(() => {
+        infoItem6.classList.toggle("visible");
+    }, 300);
+
+});
+
+close6.addEventListener("click", function infoCls(b) {
+    b.preventDefault();
+    hoverDiv6.classList.toggle("hidden");
+    infoItem6.classList.toggle("visible");
+
+});
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+
+let infoIcon7 = document.querySelector(".infoIcon7");
+let infoItem7 = document.querySelector(".info.i__7");
+
+let close7 = document.querySelector(".closeIcon7");
+let hoverDiv7 = document.querySelector(".hover.h__7")
+
+
+
+infoIcon7.addEventListener("click", function info_7(a) {
+    a.preventDefault();
+    hoverDiv7.classList.toggle("hidden");
+    setTimeout(() => {
+        infoItem7.classList.toggle("visible");
+    }, 300);
+
+});
+
+close7.addEventListener("click", function infoCls(b) {
+    b.preventDefault();
+    hoverDiv7.classList.toggle("hidden");
+    infoItem7.classList.toggle("visible");
+
+});
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+
+let infoIcon8 = document.querySelector(".infoIcon8");
+let infoItem8 = document.querySelector(".info.i__8");
+
+let close8 = document.querySelector(".closeIcon8");
+let hoverDiv8 = document.querySelector(".hover.h__8")
+
+
+
+infoIcon8.addEventListener("click", function info_8(a) {
+    a.preventDefault();
+    hoverDiv8.classList.toggle("hidden");
+    setTimeout(() => {
+        infoItem8.classList.toggle("visible");
+    }, 300);
+
+});
+
+close8.addEventListener("click", function infoCls(b) {
+    b.preventDefault();
+    hoverDiv8.classList.toggle("hidden");
+    infoItem8.classList.toggle("visible");
+
+});
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+
+let infoIcon9 = document.querySelector(".infoIcon9");
+let infoItem9 = document.querySelector(".info.i__9");
+
+let close9 = document.querySelector(".closeIcon9");
+let hoverDiv9 = document.querySelector(".hover.h__9")
+
+
+
+infoIcon9.addEventListener("click", function info_9(a) {
+    a.preventDefault();
+    hoverDiv9.classList.toggle("hidden");
+    setTimeout(() => {
+        infoItem9.classList.toggle("visible");
+    }, 300);
+
+});
+
+close9.addEventListener("click", function infoCls(b) {
+    b.preventDefault();
+    hoverDiv9.classList.toggle("hidden");
+    infoItem9.classList.toggle("visible");
+
+});
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+
+let infoIcon10 = document.querySelector(".infoIcon10");
+let infoItem10 = document.querySelector(".info.i__10");
+
+let close10 = document.querySelector(".closeIcon10");
+let hoverDiv10 = document.querySelector(".hover.h__10")
+
+
+
+infoIcon10.addEventListener("click", function info_10(a) {
+    a.preventDefault();
+    hoverDiv10.classList.toggle("hidden");
+    setTimeout(() => {
+        infoItem10.classList.toggle("visible");
+    }, 300);
+
+});
+
+close10.addEventListener("click", function infoCls(b) {
+    b.preventDefault();
+    hoverDiv10.classList.toggle("hidden");
+    infoItem10.classList.toggle("visible");
+
+});
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+
+let btn = document.querySelector('.btn', '.btn.hide', '.btn.load');
+let hiddenObj = document.querySelector(".hidden-col");
+document.querySelector('.btn').innerHTML="Load More";
+// let click = false;
+
+// function toggle() {
+//     if (!click) {
+//         click == true;
+//         btn.innerHTML= document.getElementsByClassName('.hide');
+        
+//     } 
+
+//    else {
+//         click == false;
+//         console.log('clicked')
+//         btn.innerHTML= document.getElementsByClassName('.load');
+    
+//     }
+
+// }
+
+btn.addEventListener('click', () => {
+    hiddenObj.classList.toggle("disp-block");
+    // btn.innerHTML = document.querySelector('.hide');
+    
+    // toggle();
+   
+
+
+})
+
+
